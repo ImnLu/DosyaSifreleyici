29ca77b022f9eb03365eaf79febc1971458fb21f
Mali <nlu@outlook.com.tr>
2022-03-02 17:28:56 +0300

    Add project files.

---
+++ DosyaSifreleme/Sifreleyici.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DosyaSifreleme
+{
+    partial class Sifreleyici
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sifreleyici));
+            this.textDosya = new System.Windows.Forms.TextBox();
+            this.buttonSifrele = new System.Windows.Forms.Button();
+            this.buttonSifreCoz = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDosyaSec = new System.Windows.Forms.Button();
+            this.DosyaSec = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textDosya
+            // 
+            this.textDosya.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDosya.Location = new System.Drawing.Point(60, 6);
+            this.textDosya.Name = "textDosya";
+            this.textDosya.ReadOnly = true;
+            this.textDosya.Size = new System.Drawing.Size(216, 23);
+            this.textDosya.TabIndex = 0;
+            // 
+            // buttonSifrele
+            // 
+            this.buttonSifrele.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSifrele.Location = new System.Drawing.Point(102, 33);
+            this.buttonSifrele.Name = "buttonSifrele";
+            this.buttonSifrele.Size = new System.Drawing.Size(84, 37);
+            this.buttonSifrele.TabIndex = 1;
+            this.buttonSifrele.Text = "Şifrele";
+            this.buttonSifrele.UseVisualStyleBackColor = true;
+            this.buttonSifrele.Click += new System.EventHandler(this.buttonSifre_Click);
+            // 
+            // buttonSifreCoz
+            // 
+            this.buttonSifreCoz.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSifreCoz.Location = new System.Drawing.Point(192, 33);
+            this.buttonSifreCoz.Name = "buttonSifreCoz";
+            this.buttonSifreCoz.Size = new System.Drawing.Size(84, 37);
+            this.buttonSifreCoz.TabIndex = 2;
+            this.buttonSifreCoz.Text = "Şifreyi Çöz";
+            this.buttonSifreCoz.UseVisualStyleBackColor = true;
+            this.buttonSifreCoz.Click += new System.EventHandler(this.buttonSifreCoz_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dosya:";
+            // 
+            // buttonDosyaSec
+            // 
+            this.buttonDosyaSec.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDosyaSec.Location = new System.Drawing.Point(12, 33);
+            this.buttonDosyaSec.Name = "buttonDosyaSec";
+            this.buttonDosyaSec.Size = new System.Drawing.Size(84, 37);
+            this.buttonDosyaSec.TabIndex = 4;
+            this.buttonDosyaSec.Text = "Dosyayı Seç";
+            this.buttonDosyaSec.UseVisualStyleBackColor = true;
+            this.buttonDosyaSec.Click += new System.EventHandler(this.buttonDosyaSec_Click);
+            // 
+            // DosyaSec
+            // 
+            this.DosyaSec.FileName = "DosyaSec";
+            this.DosyaSec.Title = "Dosyayı Seçin";
+            // 
+            // Sifreleyici
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 79);
+            this.Controls.Add(this.buttonDosyaSec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSifreCoz);
+            this.Controls.Add(this.buttonSifrele);
+            this.Controls.Add(this.textDosya);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Sifreleyici";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dosya Şifreleyici";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textDosya;
+        private System.Windows.Forms.Button buttonSifrele;
+        private System.Windows.Forms.Button buttonSifreCoz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDosyaSec;
+        private System.Windows.Forms.OpenFileDialog DosyaSec;
+    }
+}
+
---
+++ DosyaSifreleme/Sifreleyici.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DosyaSifreleme
+{
+    public partial class Sifreleyici : Form
+    {
+        public Sifreleyici()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSifre_Click(object sender, EventArgs e)
+        {
+            // Dosya seçilip seçilmediğini kontrol ediyor.
+            if (!string.IsNullOrEmpty(textDosya.Text))
+            {
+                // Dosyanın olup olmadığını kontrol ediyor.
+                if (File.Exists(textDosya.Text))
+                {
+                    string kontrol = textDosya.Text.Substring(textDosya.Text.Length - 8, 8);
+
+                    // Dosya şifrelenip şifrelenmediğini kontrol ediyor.
+                    if (kontrol == "mencrypt")
+                        MessageBox.Show("Bu dosya zaten şifrelenmiş!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    else
+                    {
+                        // Dosyayı okuyup her karakteri bir sonraki karaktere dönüştürüyor.
+                        Byte[] dosya = File.ReadAllBytes(textDosya.Text);
+                        for (int i = 0; i < dosya.Length; i++)
+                        {
+                            dosya[i] = (Byte)((int)dosya[i] + 1);
+                            if (dosya[i] > 255)
+                            {
+                                dosya[i] = 0;
+                            }
+                        }
+                        File.WriteAllBytes(textDosya.Text + "mencrypt", dosya);
+                        textDosya.Text = "";
+                        MessageBox.Show("Dosya başarıyla şifrelendi!", "Mesaj", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                    MessageBox.Show("Dosya bulunamadı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show("Lütfen bir dosya seçiniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void buttonSifreCoz_Click(object sender, EventArgs e)
+        {
+            // Dosyanın seçilip seçilmediğini kontrol ediyor.
+            if (!string.IsNullOrEmpty(textDosya.Text))
+            {
+                // Dosyanın olup olmadığını kontrol ediyor.
+                if (File.Exists(textDosya.Text))
+                {
+                    string kontrol = textDosya.Text.Substring(textDosya.Text.Length - 8, 8);
+                    // Dosyanın şifrelenip şifrelenmediğini kontrol ediyor.
+                    if (kontrol == "mencrypt")
+                    {
+                        // Dosyayı okuyup her karakteri bir önceki karaktere dönüştürüyor.
+                        Byte[] dosya = File.ReadAllBytes(textDosya.Text);
+                        for (int i = 0; i < dosya.Length; i++)
+                        {
+                            if (dosya[i] < 0)
+                            {
+                                dosya[i] = 255;
+                            }
+                            dosya[i] = (Byte)((int)dosya[i] - 1);
+                        }
+                        File.WriteAllBytes(textDosya.Text.Substring(0, textDosya.Text.Length - 8), dosya);
+                        textDosya.Text = "";
+                        MessageBox.Show("Dosyanın şifresi başarıyla çözüldü!", "Mesaj", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                        MessageBox.Show("Bu dosya şifrelenmiş bir dosya değil!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                    MessageBox.Show("Dosya bulunamadı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show("Lütfen bir dosya seçin!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void buttonDosyaSec_Click(object sender, EventArgs e)
+        {
+            //Dosya seçince dosya konumunu textbox'a yazdırıyor.
+            if (DosyaSec.ShowDialog() == DialogResult.OK)
+            {
+                textDosya.Text = DosyaSec.FileName;
+            }
+        }
+    }
+}
